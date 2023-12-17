618530d07a261a664f23ddcb6ecf425fd60e119e
Boris Kozlov <kozlov.99260422@gmail.com>
2023-12-17 23:40:51 +0300

    Init commit

---
+++ Array.h
@@ -0,0 +1,539 @@
+#pragma once
+
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <iostream>
+
+#define BLOCK_SIZE 16
+#define SHARED_BLOCK_SIZE 16
+#define SUMMATION_BLOCK_SIZE BLOCK_SIZE * BLOCK_SIZE
+
+#define CEILDIV(x, y) (x/y + (x % y != 0))
+
+template <typename T>
+class CPUArray {
+private:
+	size_t dim1;
+	size_t dim2;
+	T* data;
+
+public:
+
+	T& operator[] (size_t n);
+	T operator[] (size_t n) const;
+	T& operator() (size_t n, size_t m);
+	T operator() (size_t n, size_t m) const;;
+	CPUArray(size_t d1, size_t d2 = 0U);
+	CPUArray(size_t d1, size_t d2, T* dta);
+	size_t getDim1() const;
+	size_t getDim2() const;
+
+	T const* getData() const;
+	void add(const CPUArray& arr);
+	void subtract(const CPUArray& arr);
+	void print() const;
+	void fill(T** arr);
+	void fill(T* arr);
+	void setValue(T value);
+	~CPUArray() {
+		delete[] data;
+	};
+
+};
+
+
+template<typename T>
+CPUArray<T> operator+(CPUArray<T>& op1, CPUArray<T>& op2);
+
+template<typename T>
+CPUArray<T> operator*(const CPUArray<T>& op1, const CPUArray<T>& op2);
+
+template<typename T>
+class GPUArray {
+private:
+	size_t dim1;
+	size_t dim2;
+	size_t size = 0;
+	T* data;
+
+public:
+	GPUArray(const CPUArray<T>& arr);
+	GPUArray(size_t d1, size_t d2, T* arr);
+
+	void fill(const CPUArray<T>& arr);
+	void fill(T* arr);
+
+	void print() const;
+
+
+	size_t getDim1() const;
+	size_t getDim2() const;
+
+	T const* getData() const;
+
+	~GPUArray();
+
+};
+
+template<typename T>
+GPUArray<T> operator*(const GPUArray<T>& op1, const GPUArray<T>& op2);
+
+template<typename T>
+GPUArray<T> operator+(const GPUArray<T>& op1, const GPUArray<T>& op2);
+
+template<typename T>
+GPUArray<T> sharedMul(const GPUArray<T>& op1, const GPUArray<T>& op2);
+
+
+
+
+
+
+
+template <typename T>
+T& CPUArray<T>::operator[] (size_t n) {	//for dim2 = 0, i.e. vectors
+	return data[n];
+};
+
+template <typename T>
+T CPUArray<T>::operator[] (size_t n) const {	//for dim2 = 0, i.e. vectors
+	return data[n];
+};
+
+template <typename T>
+T& CPUArray<T>::operator() (size_t n, size_t m) {
+	return data[n * dim2 + m];
+}
+
+template <typename T>
+T CPUArray<T>::operator() (size_t n, size_t m) const {
+	return data[n * dim2 + m];
+};
+
+template <typename T>
+CPUArray<T>::CPUArray(size_t d1, size_t d2) {
+	dim1 = d1;
+	dim2 = d2;
+	if (dim2 == 0U)
+		data = new T[dim1];
+	else
+		data = new T[dim1 * dim2];
+}
+
+template <typename T>
+CPUArray<T>::CPUArray(size_t d1, size_t d2, T* dta) {
+	dim1 = d1;
+	dim2 = d2;
+	data = dta;
+}
+
+template <typename T>
+size_t CPUArray<T>::getDim1() const {
+	return dim1;
+}
+
+template <typename T>
+size_t CPUArray<T>::getDim2() const {
+	return dim2;
+}
+
+template <typename T>
+T const* CPUArray<T>::getData() const {
+	return data;
+}
+
+template <typename T>
+void CPUArray<T>::add(const CPUArray& arr) {
+	if (dim2 == 0) {
+		for (int i = 0; i < dim1; i++) {
+			data[i] += arr[i];
+		}
+	}
+	else {
+		for (int i = 0; i < dim1; i++) {
+			for (int j = 0; j < dim2; j++)
+			{
+				data[i * dim2 + j] += arr(i, j);
+			}
+		}
+	}
+};
+
+template <typename T>
+void CPUArray<T>::subtract(const CPUArray& arr) {
+	if (dim2 == 0) {
+		for (int i = 0; i < dim1; i++) {
+			data[i] -= arr[i];
+		}
+	}
+	else {
+		for (int i = 0; i < dim1; i++) {
+			for (int j = 0; j < dim2; j++)
+			{
+				data[i * dim2 + j] -= arr(i, j);
+			}
+		}
+	}
+};
+
+template <typename T>
+void CPUArray<T>::print() const {
+	if (dim2 == 0) {
+		std::cout << "{ ";
+		for (int i = 0; i < dim1; i++) {
+			std::cout << data[i] << " ";
+		}
+		std::cout << "}";
+	}
+	else {
+		std::cout << "[";
+		for (int i = 0; i < dim1; i++) {
+			std::cout << "[ ";
+			for (int j = 0; j < dim2; j++)
+			{
+				std::cout << data[i * dim2 + j] << " ";
+			}
+			if (i == dim1 - 1)
+				std::cout << "]";
+			else
+				std::cout << "]\n";
+		}
+		std::cout << "]";
+	}
+};
+
+template <typename T>
+void CPUArray<T>::fill(T** arr) {
+	for (int i = 0; i < dim1; i++) {
+		for (int j = 0; j < dim2; j++)
+		{
+			data[i * dim2 + j] = arr[i][j];
+		}
+	}
+};
+
+template <typename T>
+void CPUArray<T>::fill(T* arr) {
+	if (dim2 == 0) {
+		for (int i = 0; i < dim1; i++) {
+			data[i] = arr[i];
+		}
+	}
+	else {
+		for (int i = 0; i < dim1; i++) {
+			for (int j = 0; j < dim2; j++)
+			{
+				data[i * dim2 + j] = arr[i * dim2 + j];
+			}
+		}
+	}
+};
+
+template <typename T>
+void CPUArray<T>::setValue(T value) {
+	if (dim2 == 0) {
+		for (int i = 0; i < dim1; i++) {
+			data[i] = value;
+		}
+	}
+	else {
+		for (int i = 0; i < dim1; i++) {
+			for (int j = 0; j < dim2; j++)
+			{
+				data[i * dim2 + j] = value;
+			}
+		}
+	}
+}
+
+template<typename T>
+CPUArray<T> operator+(CPUArray<T>& op1, CPUArray<T>& op2) {
+	auto dim1 = op1.getDim1(), dim2 = op1.getDim2();
+	//auto sum = CPUArray<T>(dim1, dim2);
+	T* data;
+	if (dim2 == 0)
+		data = new T[dim1];
+	else
+		data = new T[dim1 * dim2];
+	if (dim2 == 0) {
+		for (int i = 0; i < dim1; i++) {
+			data[i] = op1[i] + op2[i];
+		}
+	}
+	else {
+		for (int i = 0; i < dim1; i++) {
+			for (int j = 0; j < dim2; j++)
+			{
+				data[i * dim2 + j] = op1(i, j) + op2(i, j);
+			}
+		}
+	}
+	//sum.setValue(0.);
+	return CPUArray<T>(dim1, dim2, data);
+};
+
+template<typename T>
+CPUArray<T> operator*(const CPUArray<T>& op1, const CPUArray<T>& op2) {
+	auto dim1 = op1.getDim1(), dim2 = op2.getDim2(), dim_interim = op1.getDim2();
+	//auto sum = CPUArray<T>(dim1, dim2);
+	//sum.setValue(0);
+	T* data;
+	if (dim2 == 0)
+		data = new T[dim1];
+	else
+		data = new T[dim1 * dim2];
+	if (dim2 == 0) {
+		for (int i = 0; i < dim1; i++) {
+			data[i] = op1[i] + op2[i];
+		}
+	}
+	else {
+		for (int i = 0; i < dim1; i++) {
+			for (int j = 0; j < dim2; j++)
+			{
+				T temp = 0;
+				for (int k = 0; k < dim_interim; k++) {
+					temp += op1(i, k) * op2(k, j);
+				}
+				data[i * dim2 + j] = temp;
+			}
+		}
+	}
+	return CPUArray<T>(dim1, dim2, data);
+};
+//Mat_mul << <blocks, threadsPerBlock >> > (op1.getData(), op2.getData(), data, dim1, dim2, dim_interim);
+template <typename T>
+__global__ void Mat_mul(T const* op1, T const* op2, T* res, size_t dim1, size_t dim2, size_t dim_interim) {
+	size_t i = blockIdx.y * blockDim.y + threadIdx.y;
+	size_t j = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if ((i >= dim1) || (j >= dim2))
+		return;
+
+	T sum = 0;
+	for (int k = 0; k < dim_interim; k++) {
+		sum += op1[i * dim_interim + k] * op2[k * dim2 + j];
+	}
+	res[i * dim2 + j] = sum;
+}
+
+template <typename T>
+__global__ void Sum(T const* op1, T const* op2, T* sum, int dim) {
+	auto tid = blockDim.x * blockIdx.x + threadIdx.x;
+	if (tid < dim) {
+		sum[tid] = op1[tid] + op2[tid];
+	}
+}
+
+template <typename T>
+__global__ void Mat_sum(T const* op1, T const* op2, T* res, size_t dim1, size_t dim2) {
+	size_t i = blockIdx.y * blockDim.y + threadIdx.y;
+	size_t j = blockIdx.x * blockDim.x + threadIdx.x;
+
+	if ((i >= dim1) || (j >= dim2))
+		return;
+
+	res[i * dim2 + j] = op1[i * dim2 + j] + op2[i * dim2 + j];
+}
+
+template <typename T>
+__global__ void Mat_mul_shared(T const* op1, T const* op2, T* res, size_t dim1, size_t dim2, size_t dim_interim) {
+	__shared__ T op1_tile[SHARED_BLOCK_SIZE][SHARED_BLOCK_SIZE];
+	__shared__ T op2_tile[SHARED_BLOCK_SIZE][SHARED_BLOCK_SIZE];
+
+	size_t _i = blockIdx.y * blockDim.y + threadIdx.y;
+	size_t _j = blockIdx.x * blockDim.x + threadIdx.x;
+	if ((_i >= dim1) || (_j >= dim2))
+		return;
+
+	T sum = 0;
+
+	for (int ind_tile = 0; ind_tile < CEILDIV(dim_interim, BLOCK_SIZE); ind_tile++) {
+		size_t i = blockIdx.y * blockDim.y + threadIdx.y;
+		size_t j = ind_tile * blockDim.x + threadIdx.x;
+		if ((i < dim1) && (j < dim_interim))
+		{
+			op1_tile[threadIdx.y][threadIdx.x] = op1[i * dim_interim + j];
+		}
+		else
+		{
+			op1_tile[threadIdx.y][threadIdx.x] = 0;
+		}
+		i = ind_tile * blockDim.y + threadIdx.y;
+		j = blockIdx.x * blockDim.x + threadIdx.x;
+		if ((i < dim_interim) && (j < dim2))
+		{
+			op2_tile[threadIdx.y][threadIdx.x] = op2[i * dim2 + j];
+		}
+		else
+		{
+			op2_tile[threadIdx.y][threadIdx.x] = 0;
+		}
+		__syncthreads();
+		for (int k = 0; k < SHARED_BLOCK_SIZE; k++) {
+			sum += op1_tile[threadIdx.y][k] * op2_tile[k][threadIdx.x];
+		}
+		__syncthreads();
+	}
+	res[_i * dim2 + _j] = sum;
+
+}
+
+
+template<typename T>
+GPUArray<T>::~GPUArray() {
+	cudaFree(data);
+};
+
+
+template <typename T>
+size_t GPUArray<T>::getDim1() const {
+	return dim1;
+}
+
+template <typename T>
+size_t GPUArray<T>::getDim2() const {
+	return dim2;
+}
+
+template <typename T>
+T const* GPUArray<T>::getData() const {
+	return data;
+}
+
+template <typename T>
+void GPUArray<T>::fill(const CPUArray<T>& arr) {
+	auto fillerData = arr.getData();
+	auto rc = cudaMemcpy(data, fillerData, size, cudaMemcpyHostToDevice);
+}
+
+template <typename T>
+void GPUArray<T>::fill(T* arr) {
+	auto rc = cudaMemcpy(data, arr, size, cudaMemcpyHostToDevice);
+};
+
+template <typename T>
+void GPUArray<T>::print() const {
+	if (dim2 == 0) {
+		T* buf = new T[dim1];
+		auto rc = cudaMemcpy(buf, data, size, cudaMemcpyDeviceToHost);
+		std::cout << "{ ";
+		for (int i = 0; i < dim1; i++) {
+			std::cout << buf[i] << " ";
+		}
+		std::cout << "}";
+		delete[] buf;
+	}
+	else {
+		T* buf = new T[dim1 * dim2];
+		auto rc = cudaMemcpy(buf, data, size, cudaMemcpyDeviceToHost);
+		std::cout << "[";
+		for (int i = 0; i < dim1; i++) {
+			std::cout << "[ ";
+			for (int j = 0; j < dim2; j++)
+			{
+				std::cout << buf[i * dim2 + j] << " ";
+			}
+			if (i == dim1 - 1)
+				std::cout << "]";
+			else
+				std::cout << "]\n";
+		}
+		std::cout << "]";
+		delete[] buf;
+	}
+};
+
+
+template <typename T>
+GPUArray<T>::GPUArray(const CPUArray<T>& arr) {
+	dim1 = arr.getDim1();
+	dim2 = arr.getDim2();
+	auto fillerData = arr.getData();
+	if (dim2 == 0U)
+		size = sizeof(T) * dim1;
+	else
+		size = sizeof(T) * dim1 * dim2;
+	auto rc = cudaMalloc((void**)&data, size);
+	rc = cudaMemcpy(data, fillerData, size, cudaMemcpyHostToDevice);
+}
+
+template <typename T>
+GPUArray<T>::GPUArray(size_t d1, size_t d2, T* arr) {
+	dim1 = d1;
+	dim2 = d2;
+	data = arr;
+	if (dim2 == 0)
+		size = dim1 * sizeof(T);
+	else
+		size = dim1 * dim2 * sizeof(T);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+template<typename T>
+GPUArray<T> operator*(const GPUArray<T>& op1, const GPUArray<T>& op2) {
+	size_t dim1 = op1.getDim1(), dim2 = op2.getDim2(), dim_interim = op1.getDim2();
+	T* data;
+	auto rc = cudaMalloc((void**)&data, dim1 * dim2 * sizeof(T));
+	dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
+	dim3 blocks(CEILDIV(dim1, BLOCK_SIZE), CEILDIV(dim2, BLOCK_SIZE));
+	T* buffer = new T[dim1 * dim2];
+	cudaMemcpy(buffer, data, dim1 * dim2 * sizeof(T), cudaMemcpyDeviceToHost);
+	Mat_mul << <blocks, threadsPerBlock >> > (op1.getData(), op2.getData(), data, dim1, dim2, dim_interim);
+	rc = cudaDeviceSynchronize();
+	cudaMemcpy(buffer, data, dim1 * dim2 * sizeof(T), cudaMemcpyDeviceToHost);
+
+	return GPUArray<T>(dim1, dim2, data);
+};
+
+template<typename T>
+GPUArray<T> operator+(const GPUArray<T>& op1, const GPUArray<T>& op2) {
+	size_t dim1 = op1.getDim1(), dim2 = op2.getDim2();
+	T* data;
+	if (dim2 == 0) {
+		auto rc = cudaMalloc((void**)&data, dim1 * sizeof(T));
+		dim3 blocks(CEILDIV(dim1, SUMMATION_BLOCK_SIZE));
+		int num_of_threads = 0;
+		if (dim1 <= SUMMATION_BLOCK_SIZE)
+			num_of_threads = dim1;
+		else
+			num_of_threads = SUMMATION_BLOCK_SIZE;
+		dim3 threadsPerBlock(num_of_threads);
+		Sum << <blocks, threadsPerBlock >> > (op1.getData(), op2.getData(), data, dim1);
+		rc = cudaDeviceSynchronize();
+	}
+	else {
+		auto rc = cudaMalloc((void**)&data, dim1 * dim2 * sizeof(T));
+		dim3 threadsPerBlock(BLOCK_SIZE, BLOCK_SIZE);
+		dim3 blocks(CEILDIV(dim1, BLOCK_SIZE), CEILDIV(dim2, BLOCK_SIZE));
+		Mat_sum << <blocks, threadsPerBlock >> > (op1.getData(), op2.getData(), data, dim1, dim2);
+		rc = cudaDeviceSynchronize();
+	}
+
+	return GPUArray<T>(dim1, dim2, data);
+};
+
+template<typename T>
+GPUArray<T> sharedMul(const GPUArray<T>& op1, const GPUArray<T>& op2) {
+	size_t dim1 = op1.getDim1(), dim2 = op2.getDim2(), dim_interim = op1.getDim2();
+	T* data;
+	auto rc = cudaMalloc((void**)&data, dim1 * dim2 * sizeof(T));
+	dim3 threadsPerBlock(SHARED_BLOCK_SIZE, SHARED_BLOCK_SIZE);
+	dim3 blocks(CEILDIV(dim1, SHARED_BLOCK_SIZE), CEILDIV(dim2, SHARED_BLOCK_SIZE));
+
+	Mat_mul_shared << <blocks, threadsPerBlock >> > (op1.getData(), op2.getData(), data, dim1, dim2, dim_interim);
+	rc = cudaDeviceSynchronize();
+
+	return GPUArray<T>(dim1, dim2, data);
+};
---
+++ kernel.cu
@@ -0,0 +1,139 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <iostream>
+//#include "FileName.cu"
+#include "Array.h"
+#include <random>
+#include <chrono>
+#include <vector>
+#include <numeric>
+void VectorTest() {
+	int dim = 3;
+	auto cpuVec1 = CPUArray<double>(dim);
+	auto cpuVec2 = CPUArray<double>(dim);
+    cpuVec1[0] = -1.135;
+    cpuVec1[1] = 12.67;
+    cpuVec1[2] = 41.135;
+    cpuVec2[0] = 9.7;
+    cpuVec2[1] = 3.;
+    cpuVec2[2] = 5.81;
+    std::cout << "CPU Vectors sum:" << std::endl;
+    cpuVec1.print();
+    std::cout << " + ";
+    cpuVec2.print();
+    std::cout << " = ";
+	(cpuVec1 + cpuVec2).print();
+    std::cout << std::endl << "GPU Vectors sum:" << std::endl;
+    auto gpuVec1 = GPUArray<double>(cpuVec1);
+    auto gpuVec2 = GPUArray<double>(cpuVec2);
+    gpuVec1.print();
+    std::cout << " + ";
+    gpuVec2.print();
+    std::cout << " = ";
+    (gpuVec1 + gpuVec2).print();
+    std::cout << std::endl;
+
+}
+
+template<typename T, int TYPE_NUM>
+void FillMatrix(T* mat, int dim1, int dim2) {
+    std::random_device rd;  // a seed source for the random number engine
+    std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
+    if (TYPE_NUM == 0) {
+        std::uniform_int_distribution<> distrib(-10, 10);
+        for (int i = 0; i < dim1; i++) {
+            for (int j = 0; j < dim2; j++)
+            {
+                mat[i * dim2 + j] = distrib(gen);
+            }
+        }
+    }
+    else {
+        std::uniform_real_distribution<> distrib(-10, 10);
+        for (int i = 0; i < dim1; i++) {
+            for (int j = 0; j < dim2; j++)
+            {
+                mat[i * dim2 + j] = distrib(gen);
+            }
+        }
+
+    }
+
+}
+
+template<typename T, int TYPE_NUM>
+void CompareMethods(int dim = 100, int reps = 1) {
+    T* A_raw = new T[dim * dim];
+    T* B_raw = new T[dim * dim];
+    auto A = CPUArray<T>(dim, dim);
+    auto B = CPUArray<T>(dim, dim);
+    A.setValue((T)0);
+    B.setValue((T)0);
+    auto A_gpu = GPUArray<T>(A);
+    auto B_gpu = GPUArray<T>(B);
+    long long naiveDur = 0;
+    long long GPUDur = 0;
+    long long GPUSharedDur = 0;
+    auto begin = std::chrono::high_resolution_clock::now();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::vector<long long> naiveDurs;
+    std::vector<long long> GPUDurs;
+    std::vector<long long> GPUSharedDurs;
+    for (int i = 0; i < reps; i++) {
+        FillMatrix<T, TYPE_NUM>(A_raw, dim, dim);
+        FillMatrix<T, TYPE_NUM>(B_raw, dim, dim);
+        A.fill(A_raw);
+        B.fill(B_raw);
+        A_gpu.fill(A_raw);
+        B_gpu.fill(B_raw);
+
+        if (i == 0) {
+            begin = std::chrono::high_resolution_clock::now();
+            auto C = A * B;
+            end = std::chrono::high_resolution_clock::now();
+            naiveDur = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+            naiveDurs.push_back(naiveDur);
+        }
+        begin = std::chrono::high_resolution_clock::now();
+        auto C_gpu = A_gpu * B_gpu;
+        end = std::chrono::high_resolution_clock::now();
+        GPUDur = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+        GPUDurs.push_back(GPUDur);
+
+        begin = std::chrono::high_resolution_clock::now();
+        auto C_gpu_shared = sharedMul(A_gpu, B_gpu);
+        end = std::chrono::high_resolution_clock::now();
+        GPUSharedDur = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+        GPUSharedDurs.push_back(GPUSharedDur);
+        //std::cout << C_gpu(0, 0) << C_gpu(3, 5) << C_gpu_shared(0, 0) << C_gpu_shared(3, 5);
+    }
+    long long sum = std::accumulate(naiveDurs.begin(), naiveDurs.end(), 0.0);
+    double mean = sum / naiveDurs.size();
+    std::cout << sum << "ns total, average : " << mean / reps << "ns." << std::endl;
+    for (int i = 0; i < naiveDurs.size(); i++)
+        std::cout << naiveDurs[i] << std::endl;
+    std::cout << std::endl;
+    sum = std::accumulate(GPUDurs.begin(), GPUDurs.end(), 0.0);
+    mean = sum / GPUDurs.size();
+    std::cout << sum << "ns total, average : " << mean / reps << "ns." << std::endl;
+    for (int i = 0; i < GPUDurs.size(); i++)
+        std::cout << GPUDurs[i] << std::endl;
+    std::cout << std::endl;
+    sum = std::accumulate(GPUSharedDurs.begin(), GPUSharedDurs.end(), 0.0);
+    mean = sum / GPUSharedDurs.size();
+    std::cout << sum << "ns total, average : " << mean / reps << "ns." << std::endl;
+    for (int i = 0; i < GPUSharedDurs.size(); i++)
+        std::cout << GPUSharedDurs[i] << std::endl;
+    std::cout << std::endl;
+}
+
+
+
+int main() {
+    VectorTest();
+    CompareMethods<int, 0>(500, 100);
+	return 0;
+}
